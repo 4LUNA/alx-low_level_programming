0c8aba5a1d63f3a0bdad1351bcb67b5744f0d638
4LUNA <oluwadamilolaolagoroye@yahoo.com>
2023-09-07 13:52:37 -0700

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,12 +3,13 @@
 /**
  * main - Entry point for the program
  *
- * This program prints a line using the puts function.
+ * Description: This program prints a message using the puts function.
  *
- * Return: 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-    puts("Programming is like building a multilingual puzzle\n");
-    return (0);
+puts("Programming is like building a multilingual puzzle\n");
+return (0);
 }
+
