da7877c01fa29b5ce57a00b193da9fb12878abc0
4LUNA <oluwadamilolaolagoroye@yahoo.com>
2023-09-07 12:53:53 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point for the program
+ *
+ * This program prints the specified text with proper grammar
+ * and the outcome is a piece of art, followed by a new line.
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
