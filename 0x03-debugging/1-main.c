c18dcf20410e582418e1c877274e91460e8335e8
4LUNA <oluwadamilolaolagoroye@yahoo.com>
2023-09-14 14:57:38 -0700

    Like, comment, subscribe

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,20 +4,20 @@
  * main - causes an infinite loop
  * Return: 0
  */
+
 int main(void)
-{
 int i;
 
 printf("Infinite loop incoming :(\n");
+
 i = 0;
 
-/* Comment out the loop to prevent infinite loop */
-/*
 while (i < 10)
 {
-putchar(i);
+putchar(i + '0'); // Convert the integer to a character
+i++;
 }
-*/
+
 printf("Infinite loop avoided! \\o/\n");
 
 return (0);
