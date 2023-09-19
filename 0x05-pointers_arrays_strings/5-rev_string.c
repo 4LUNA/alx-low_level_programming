1027bf27dfe2c8d17fd5c0d984e75ed436cc0457
4LUNA <oluwadamilolaolagoroye@yahoo.com>
2023-09-19 16:52:32 -0700

    Reverses

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - Reverses a string
+ * @s: Pointer to the string
+ */
+void rev_string(char *s)
+{
+int length = 0;
+int i, j;
+char temp;
+
+/*Calculate the length of the string*/
+
+while (s[length] != '\0')
+length++;
+ 
+/*Reverse the string*/
+for (i = 0, j = length - 1; i < j; i++, j--)
+{
+temp = s[i];
+s[i] = s[j];
+s[j] = temp;
+}
+}
